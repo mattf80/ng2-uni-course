c89cac97345772d93c546d30a57b2d949cff3e48
Matt Frowe <frowem@ucles.internal>
2016-11-25 09:07:52 +0000

    cleared git SHA1s

--- src/app/wines/wine-list/wine-list.component.ts
+++ src/app/wines/wine-list/wine-list.component.ts
@@ -6,16 +6,13 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   templateUrl: './wine-list.component.html'
 })
 export class WineListComponent implements OnInit {
-<<<<<<< HEAD
+
   wines: Wine[] = [
     new Wine('Latour', 'French plonk', 'http://www.hotel-r.net/im/hotel/fr/ch%C3%A2teau-latour.png', []),
     new Wine('Margaux', '1st Growth', 'http://www.hotel-r.net/im/hotel/fr/ch%C3%A2teau-latour.png', [])
   ];
-=======
-  wines: Wine[] = [];
+
   @Output() wineSelected = new EventEmitter<Wine>();
-  wine = new Wine('Latour', 'French plonk', 'http://www.hotel-r.net/im/hotel/fr/ch%C3%A2teau-latour.png')
->>>>>>> a6f3f3650f67e552a6617ad553e8485d99b6cc65
 
   constructor() { }
 
